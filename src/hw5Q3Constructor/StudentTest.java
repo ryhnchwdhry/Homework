527d159d4d0875d0a3d7b858ce1eb7e84e901028
ryhnchwdhry <104733459+ryhnchwdhry@users.noreply.github.com>
2022-05-19 18:50:42 -0400

    HomeWork02

---
+++ src/hw5Q3Constructor/StudentTest.java
@@ -0,0 +1,9 @@
+package hw5Q3Constructor;
+
+public class StudentTest {
+	public static void main(String[] args) {
+
+		Student student = new Student();
+		Student student1 = new Student("Rayhan Chowdhury, ", 20220237, 'M', true, 3.80f);
+	}
+}
