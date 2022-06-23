6807d4ec088b8c984a3f861d4fbd2618d9a0c869
ryhnchwdhry <104733459+ryhnchwdhry@users.noreply.github.com>
2022-06-23 18:17:31 -0400

    hw13

---
+++ src/hw13/Hw_13.java
@@ -0,0 +1,71 @@
+package hw13;
+
+public class Hw_13 {
+	/*
+	 * 1st way five example bellow:- //input[@id='twotabsearchtextbox']
+	 * //input[@type='submit'] //i[@class='hm-icon nav-sprite']
+	 * //select[@style='display: block;'] //span[@class='icp-nav-link-inner']
+	 */
+
+	/*
+	 * Second way five example:- //i[@class='a-icon a-icon-logo' or @role='img']
+	 * //input[@type='text' or @name='field-keywords'] //input[@maxlength='128'
+	 * or @tabindex='1'] //label[@for='ap_email' or @class='a-form-label']
+	 * //div[@id='auth-external-javascript' or @id='auth-external-javascript']
+	 */
+
+	/*
+	 * third way five example:- //div[@class='_p13n-zg-banner-landing-page-
+	 * header_style_zgLandingPageBanner__15GAl'
+	 * and @id='CardInstanceFqjKjOXMu_JRMvdyhwLuOg'] //input[@type='hidden'
+	 * and @id='issprefix'] //form[@action='/s/ref=nb_sb_noss'
+	 * and @id='nav-search-bar-form'] //header[@class='nav-opt-sprite nav-flex
+	 * nav-locale-us nav-lang-en nav-ssl nav-unrec nav-progressive-attribute'
+	 * and @id='navbar-main'] //script[@mark='aboveNavInjectionJS'
+	 * and @type='text/javascript']
+	 */
+
+	/*
+	 * Fourth way five example:- //span[text()='Returns'] //a[text()='Computers']
+	 * //a[text()='Health & Household'] //a[text()='Smart Home'] //a[text()='Pet
+	 * Supplies']
+	 */
+
+	/*
+	 * Fifth way five example:- //h2[contains(text(), 'Update')]
+	 * //span[contains(text(), 'Best Sellers in Computers')] //h2[contains(text(),
+	 * 'Prep')] //h2[contains(text(), 'Shop by ')] //h2[contains(text(), 'Save $150
+	 * on Ech')]
+	 */
+
+	/*
+	 * six way five example:- //div[contains(@class, 'a-section a-spacing-none
+	 * feed-carousel ')] //h1[contains(@class, 'a-size-l')] //h1[contains(@class,
+	 * 'a-size-lar')] //input[contains(@id, 'twotabs')] //input[contains(@id,
+	 * 'issp')]
+	 */
+
+	/*
+	 * seven way five example:- Didn't find any website that contains abnormal
+	 * space.
+	 */
+
+	/*
+	 * Eight way five example:- //p[starts-with(text(),'Enjoy the freedom')]
+	 * //p[starts-with(text(),'Our community is growing. M&T and People’')]
+	 * //p[starts-with(text(),'This year’s letter explores the impact of the
+	 * pandemic')] //p[starts-with(text(),'You can connect with a')]
+	 * //p[starts-with(text(),' are available today ')]
+	 */
+
+	/*
+	 * Nine way five example:- //sup[starts-with(@class,'n')]
+	 * //div[starts-with(@data-a-card-type, 'basic')] //ul[starts-with(@class,
+	 * 'a-unordered-list a-vertical a-spacing-m')] //span[starts-with(@class,
+	 * 'a-size-large p')] //span[starts-with(@class, 'a-size-large p')]
+	 * 
+	 * 
+	 * 
+	 */
+
+}
